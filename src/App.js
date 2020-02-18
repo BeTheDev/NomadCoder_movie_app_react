8466b20b4501d83ad815b8f01a5d32e08110bee3
daonez <cyongchoi@gmail.com>
2020-02-18 20:58:13 +0900

    practice of react basic

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//?? FUNCTION PROPS
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// import React from "react";
+// //import Potato from "./Potato";
+// import PropTypes from "prop-types";
 
-export default App;
+// const foodILike = [
+//   {
+//     id: 1,
+//     name: "kimchi",
+//     image: "https://www.maangchi.com/wp-content/uploads/2019/11/vegankimchi-insta.jpg",
+//     rating: 4.5
+//   },
+//   {
+//     id: 2,
+//     name: "kimbap",
+//     image: "https://i1.wp.com/jajabakes.com/wp-content/uploads/2018/08/cut-kimbab-recipe-1.jpg?w=860",
+//     rating: 5.0
+//   }
+// ];
+// //!basic
+// // function App() {
+// //   return (
+// //     <div>
+// //       <h1> Hello </h1>
+// //       <Food fav="kimchi" />
+// //       {/*food component value of kimchi
+// //       property value of kimchi(boolean/array)  */}
+// //       <Food fav="ramen"/>
+// //       <Food fav="bibimbap" />
+// //       <Food fav="friedRice" />
+
+// //     </div>
+// //   );
+// // }
+
+// // function renderFood(dish) {
+// //   return <Food name={dish.name} picture={dish.image} />;
+// // }
+
+// function Food({ name, picture }) {
+//   //console.log(props.fav);
+//   return (
+//     <div>
+//       <h2>I like {name}</h2>
+//       <img src={picture} alt={name} />
+//     </div>
+//   );
+// }
+// Food.propTypes = {
+//   name: PropTypes.string.isRequired,
+//   picture: PropTypes.string.isRequired,
+//   rating: PropTypes.number.isRequired
+// };
+
+// //! making dynamic
+// function App() {
+//   return (
+//     <div>
+//       {/* return <div>{foodILike.map(renderFood)}</div> */}
+//       {foodILike.map(dish => (
+//         <Food name={dish.name} picture={dish.image} key={dish.id} rating={dish.rating} />
+//       ))}
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ src/App1.js
@@ -0,0 +1,33 @@
+//!!! State
+
+// import React from "react";
+// //import Potato from "./Potato";
+// import PropTypes from "prop-types";
+// import axios from "axios";
+
+// class App1 extends React.Component {
+//   state = {
+//     count: 0
+//   };
+//   add = () => {
+//     this.setState(current => ({
+//       count: current.count + 1
+//     }));
+//   };
+//   minus = () => {
+//     this.setState(current => ({
+//       count: current.count - 1
+//     }));
+//   };
+//   render() {
+//     return (
+//       <div>
+//         <h1>The number is: {this.state.count}</h1>
+//         <button onClick={this.add}>Add</button>
+//         <button onClick={this.minus}>Minus</button>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App1;
